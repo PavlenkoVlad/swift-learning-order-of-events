df267d9a78b86bf8ac013d990a918ae18b74d8f4
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-07-01 18:03:00 +0300

    Initial Commit

---
+++ OrderOfEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderOfEvents
+//
+//  Created by Владислав Павленко on 7/1/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
